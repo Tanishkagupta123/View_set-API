06c086f0f54436decae05237cc6d60dc11366dd9
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-03-27 01:14:08 +0530

    update

--- app/views.py
+++ app/views.py
@@ -5,7 +5,6 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
-
 class StudentViewSet(viewsets.ViewSet):
 
     def list(self, request):
