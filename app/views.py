804675c807b0386112d02be533615724947baa67
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-03-29 20:21:12 +0530

    update

--- app/views.py
+++ app/views.py
@@ -48,10 +48,10 @@ class StudentViewSet(viewsets.ViewSet):
 
 # Short code
 
-class StudentViewSet(viewsets.ModelViewSet):
-    """
-    A simple ViewSet for viewing and editing accounts.
-    """
-    queryset = Student.objects.all()
-    serializer_class = StuSerializer
+# class StudentViewSet(viewsets.ModelViewSet):
+#     """
+#     A simple ViewSet for viewing and editing accounts.
+#     """
+#     queryset = Student.objects.all()
+#     serializer_class = StuSerializer
     
